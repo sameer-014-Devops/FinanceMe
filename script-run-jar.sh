ce96a150b8186c1dbe5886092684af1243417a66
Sameer A <sksameer4970@gmail.com>
2025-02-05 14:35:41 +0530

    Update script-run-jar.sh

--- script-run-jar.sh
+++ script-run-jar.sh
@@ -2,11 +2,11 @@
 
 # Automatically select the first JAR file in the directory
 Wrk_Dir="/opt/tomcat/webapps"
-JAR_FILE="$Wrk_Dir/$(ls financeme-*.jar 2>/dev/null | sort -V | tail -n 1)"
+JAR_FILE=$(ls "$Wrk_Dir"/financeme-*.jar 2>/dev/null | sort -V | tail -n 1)
 PID_FILE="$Wrk_Dir/app.pid"
 
 # Check if a JAR file exists
-if [ ! -f "$JAR_FILE" ]; then
+if [ -z "$JAR_FILE" ]; then
     echo "No JAR file found in the current directory."
     exit 1
 fi
